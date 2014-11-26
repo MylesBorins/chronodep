e3ee41d93e4f34370f156e0b195c2bc5a9984f81
Myles Borins <myles@famo.us>
2014-11-26 00:00:49 -0800

    First pass at reduce

--- test/test-lib.js
+++ test/test-lib.js
@@ -1,12 +1,22 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+'use strict';
+
+var path = require('path');
 
 var test = require('tape');
 
 var chronodep = require('../lib');
 
+var root = path.join(__dirname, '..');
+
 test('Does Myles like rent?', function (t) {
-  t.plan(1);
-  t.equal(chronodep, 'I really love the play rent\nI can recite the first couple minutes of it', 'of course he does!');
+  t.plan(2);
+  chronodep(root, function (err, deps) {
+    t.error(err, 'the callback should be called without an error');
+    console.log('Final outoutput:');
+    console.log(deps);
+    t.equal(deps.constructor, Array, 'the return should be an array');
+  });
 });
